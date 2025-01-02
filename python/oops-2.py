9c340aea5495ee0d65f5e82b2570531b63b47619
balajibheesetti <balaji9010215083@gmail.com>
2025-01-02 19:29:50 +0530

    turtle

--- python/oops-2.py
+++ python/oops-2.py
@@ -16,26 +16,25 @@
 # a =App(20,"main")
 # a.login()
 # a.increase_capacity(20)
-
 # b = App(30,"balaji")
 # b.login()
 # b.increase_capacity(40)
 
-class OnlineClass:
-    def __init__(self,course,instructor):
-        self.course = course
-        self.instructor = instructor
-        self.students = []
+# class OnlineClass:
+#     def __init__(self,course,instructor):
+#         self.course = course
+#         self.instructor = instructor
+#         self.students = []
 
-    def enroll_student(self,name):
-        self.students.append(name)
-        print(f"{name} has been enrolled to {self.course} !")
-    
-    def course_details(self):
-        print(f"course :{self.course}")
-        print(f"instructor : {self.instructor}")
-        print(f"enrolled students = {self.students}")
+#     def enroll_student(self,name):
+#         self.students.append(name)
+#         print(f"{name} has been enrolled to {self.course} !")
+   
+#     def course_details(self):
+#         print(f"course :{self.course}")
+#         print(f"instructor : {self.instructor}")
+#         print(f"enrolled students = {self.students}")
 
-st1 = OnlineClass("c","balaji")
-st1.enroll_student("hari")
-st1.course_details()
+# st1 = OnlineClass("c","balaji")
+# st1.enroll_student("hari")
+# st1.course_details()
